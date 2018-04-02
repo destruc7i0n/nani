b6aa46cc08dfd13c4fe67cdc3dcce7664dc490ef
destruc7i0n <dscdsouza@outlook.com>
2018-04-01 23:13:24 -0400

    Proxy Netlify API requests

--- src/lib/useProxy.js
+++ src/lib/useProxy.js
@@ -2,7 +2,7 @@ const CRUNCHYROLL_CDN = 'http://img1.ak.crunchyroll.com/'
 
 export default (url) => {
   if (process.env.NODE_ENV === 'production') {
-    return url.replace(CRUNCHYROLL_CDN, '/proxy/')
+    return url.replace(CRUNCHYROLL_CDN, '/proxy/image/')
   }
   return url
 }
