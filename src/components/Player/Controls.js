1f2f5c039c913767e6f8257dbe03a76d6948aa96
destruc7i0n <dscdsouza@outlook.com>
2020-09-23 22:07:50 -0400

    update speeds

--- src/components/Player/Controls.js
+++ src/components/Player/Controls.js
@@ -256,10 +256,14 @@ class Controls extends Component {
                 <div className='col'>Speed</div>
                 <div className='col'>
                   <select name='speed' id='speed-selector' value={speed} onChange={(e) => setSpeed(e.target.value)}>
-                    <option value={0.5}>0.5x</option>
-                    <option value={1}>1x</option>
-                    <option value={1.5}>1.5x</option>
                     <option value={2}>2x</option>
+                    <option value={1.75}>1.75x</option>
+                    <option value={1.5}>1.5x</option>
+                    <option value={1.25}>1.25x</option>
+                    <option value={1}>1x</option>
+                    <option value={0.75}>0.75x</option>
+                    <option value={0.5}>0.5x</option>
+                    <option value={0.25}>0.25x</option>
                   </select>
                 </div>
               </div>
